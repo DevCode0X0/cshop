d167efa7cc99ecdd71d5cd6322aae53576852c87
DevCode0X0 <devcode.official01@gmail.com>
2024-09-10 14:23:28 +0700

    Hello World!

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
+
+//export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
